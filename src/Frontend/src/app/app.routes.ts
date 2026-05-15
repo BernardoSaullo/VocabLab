6d8ff1b09fdeb23a740f6bf3cd041803f4f6d803
Bernardo-Saullo-Atlas <bernardo.costa@atlasinovacoes.com.br>
2026-05-15 15:40:57 -0300

    feat: implement main layout component with nav-bar

--- src/Frontend/src/app/app.routes.ts
+++ src/Frontend/src/app/app.routes.ts
@@ -2,7 +2,13 @@ import { Routes } from '@angular/router';
 import { BaseContainerComponent } from './shared/components/base-container/base-container.component';
 import { HomeComponent } from './modules/home/home.component';
 import { InputIconComponent } from './shared/components/inputs/input-icon/input-icon.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 export const routes: Routes = [
-	{ path: '', component: HomeComponent },
+
+	{ path: '', component: MainLayoutComponent,
+    children: [
+      {path: '', component: HomeComponent}
+    ]
+  },
 	{ path: 'test-component', component: InputIconComponent },
 ];
