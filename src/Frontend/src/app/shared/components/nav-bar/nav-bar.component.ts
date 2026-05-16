8b89cefe0c70095b6eaea5061670a96ce39665fd
Bernardo-Saullo-Atlas <bernardo.costa@atlasinovacoes.com.br>
2026-05-16 20:29:10 -0300

    feat: create logo component

--- src/Frontend/src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/Frontend/src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { AvatarModule } from 'primeng/avatar';
 import { OverlayBadgeModule } from 'primeng/overlaybadge';
+import { LogoComponent } from "../logo/logo.component";
 
 @Component({
   selector: 'app-nav-bar',
   imports: [
     AvatarModule,
-    OverlayBadgeModule
-  ],
+    OverlayBadgeModule,
+    LogoComponent
+],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
