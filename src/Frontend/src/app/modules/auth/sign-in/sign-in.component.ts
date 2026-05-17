7540b889903f2a5ef6a59f0321790af16d975933
Bernardo-Saullo-Atlas <bernardo.costa@atlasinovacoes.com.br>
2026-05-17 00:10:24 -0300

    refactor: ajust page for use base-container

--- src/Frontend/src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/Frontend/src/app/modules/auth/sign-in/sign-in.component.ts
@@ -4,16 +4,19 @@ import { PasswordModule } from 'primeng/password';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { LogoComponent } from "../../../shared/components/logo/logo.component";
-
+import { RouterLink } from '@angular/router';
+import { BaseContainerComponent } from '../../../shared/components/base-container/base-container.component';
 
 @Component({
   selector: 'app-sign-in',
   imports: [
+    BaseContainerComponent,
     FormsModule,
     PasswordModule,
     InputTextModule,
     ButtonModule,
-    LogoComponent
+    LogoComponent,
+    RouterLink
 ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
