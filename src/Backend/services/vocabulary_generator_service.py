67b092d8b93438405a8948eaca9fafc293af019c
Bernardo-Saullo-Atlas <bernardo.costa@atlasinovacoes.com.br>
2026-06-08 00:44:48 -0300

    feat: implement VocabularyGeneratorService for generating vocabulary based on user input

---
+++ src/Backend/services/vocabulary_generator_service.py
@@ -0,0 +1,62 @@
+import asyncio
+import json
+import os
+import re
+from groq import AsyncGroq
+from services.prompt_service import PromptService
+
+
+class VocabularyGeneratorService:
+    async def generate(self, words, input_language, output_language, num_words=10, context="Daily life"):
+        system_prompt = PromptService().render(
+            "system",
+            {
+                "input_language": input_language,
+                "output_language": output_language,
+                "num_words": str(num_words),
+                "context": context,
+            },
+        )
+        api_key=os.environ.get("GROQ_API_KEY")
+        print(api_key)
+
+        client = AsyncGroq(
+            api_key=os.environ.get("GROQ_API_KEY"),
+            )
+        
+
+        chat_completion = await client.chat.completions.create(
+    
+            messages=[
+                {
+                    "role": "system",
+                    "content": system_prompt,
+                },
+
+                {
+                    "role": "user",
+                    "content": f"{words}"
+                }
+            ],
+
+            model="llama-3.3-70b-versatile",
+
+            temperature=0.5,
+
+            max_completion_tokens=1024,
+
+            top_p=1,
+
+            stop=None,
+
+            stream=False,
+        )
+        data = chat_completion.choices[0].message.content
+        array_match = re.search(r'\[\s*{.*?}\s*]', data, re.DOTALL)
+
+        
+        if array_match:
+            print(json.loads(array_match.group()))
+            return
+        
+            
