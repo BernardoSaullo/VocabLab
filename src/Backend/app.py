554e1b9ab0ca3e239c711df38c034a3f1202b3ec
Bernardo-Saullo-Atlas <bernardo.costa@atlasinovacoes.com.br>
2026-06-08 10:28:51 -0300

    feat: add launch configuration for FastAPI debugging

--- src/Backend/app.py
+++ src/Backend/app.py
@@ -1,20 +1,30 @@
-import asyncio
+# import asyncio
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-from services.vocabulary_generator_service import VocabularyGeneratorService
+# from services.vocabulary_generator_service import VocabularyGeneratorService
 
-async def main():
-    load_dotenv()
-    vocab_generator = VocabularyGeneratorService()
-    await vocab_generator.generate(
-        words="apple, book, run",
-        input_language="English",
-        output_language="Portuguese",
-        num_words=10,
-        context="Daily life",
-    )
+# async def main():
+#     load_dotenv()
+#     vocab_generator = VocabularyGeneratorService()
+#     await vocab_generator.generate(
+#         words="apple, book, run",
+#         input_language="English",
+#         output_language="Portuguese",
+#         num_words=10,
+#         context="Daily life",
+#     )
 
 
 
-asyncio.run(main())
+# asyncio.run(main())
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    teste = "ratatui"
+    return {"message": teste}
